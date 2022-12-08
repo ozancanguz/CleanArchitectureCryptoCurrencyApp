0be1870127c7434178ed7774f952749442d72590
Ozan <ozancangu24@gmail.com>
2022-12-08 17:22:42 +0300

    -di part HiltApplication.kt created

---
+++ app/src/main/java/com/ozancanguz/mvmmcurrenciesforusd/HiltApplication.kt
@@ -0,0 +1,8 @@
+package com.ozancanguz.mvmmcurrenciesforusd
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class HiltApplication : Application() {
+}
